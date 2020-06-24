0d4975777f33acfbc6b6cedd1fb001d9f2493a7d
Simran Jain <itsimmi5@gmail.com>
2020-06-24 01:19:40 -0400

    feat: added feedback form including dropdown selection for employee name, text box for feedback from api error: dropdown items are not showing check line= 44

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,54 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {fetchEmployeeNames} from './apis/employee';
 
 export default class Home extends Component {
-    render() {
+    constructor(){
+        super();
+        this.state = {
+            name:'',
+            feedback:''
+        }
+    }
+
+    componentDidMount() {
+        let nameList = [];
+        fetchEmployeeNames()
+        .then(response => {
+            Object.keys(response).forEach(e => {
+                nameList.push(e.employee_name)
+            });
+        })
+        this.setState({
+            name: nameList
+        });
+    }
+
+    saveFeedback = (e) => {
+        e.preventDefault();
+        console.log('submit feedback working');
+    }
+
+    nameHandleChange(event) {
+        this.setState({name: event.target.value});
+      }
+
+    feedbackHandleChange(event) {
+        this.setState({feedback: event.target.value});
+      }
+        render() {
         return (
             <div>
-                hi there
+                select name 
+                {/* <textarea value={this.state.name} onChange={this.nameHandleChange}/> */}
+
+                <select>
+                    {Object.keys(this.state.name).map((ename,index) => (
+                        <option value={ename} key={index}> {ename} </option>
+                    ))}
+                </select>
+                Feedback 
+                <textarea value={this.state.feedback} onChange={this.feedbackHandleChange}/>
+                <button> Submit </button>
             </div>
         )
     }
